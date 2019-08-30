21692b45d6f9008d3aa75e6b6db2ab29bbad0d0a
Mina Farag <minaf1@outlook.com>
2019-08-30 00:48:50 +0000

    added my own function

--- web-content/js/revantarctica.js
+++ web-content/js/revantarctica.js
@@ -4,7 +4,7 @@ let dbObject = {
     techTrack:''
 }
 
-document.getElementById('header').innerText = "YOUR TITLE GOES HERE";
+document.getElementById('header').innerText = "Revantarctica Unlimited!";
 
 //this assumes your cloud function will return a value named address with the address to an image, in a cloud storage bucket
 async function setUpImages(){
@@ -40,6 +40,7 @@ async function setUpImages(){
 }
 setUpImages()
 
+
 document.getElementById('calc-label').innerText = "Enter a number"
 document.getElementById('calc-input').type = 'number'
 
@@ -67,6 +68,37 @@ async function calcSubmit(event){
     let p = document.getElementById('answer')
     p.innerText = 'Your result is: ' + data.toString()
 }
+
+
+
+document.getElementById('myFunction-label').innerText = "Enter a number"
+document.getElementById('calc-label').innerText = "Enter a number"
+async function calcMyFunction(event){
+	event.preventDefault()
+	let result = await fetch("https://us-central1-cloudadmingcpdemosmina.cloudfunctions.net/get_primes", {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({"body": document.getElementById('myFunction-input').value})
+	}).then(r => r.json());
+
+	console.log('result:' + result)
+	if(document.getElementById('myFunction-input').type === 'number'){
+		document.getElementById('myFunction-input').value = 0
+	}else {
+		document.getElementById('myFunction-input').value = ''
+	}
+	
+	let data = await result.toString()
+	console.log('data:'+data)
+
+	let p = document.getElementById('myFunction-paragraph')
+	p.innerText = 'Primes:' + data.toString()
+}
+
+
+
 
 
 
